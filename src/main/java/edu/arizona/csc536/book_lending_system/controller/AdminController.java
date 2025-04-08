28a9e4ecb1e282cc30300d05a7c5359cacec14b2
BourBon_TT <57243066+TT159@users.noreply.github.com>
2025-04-08 01:38:55 -0700

    Update AdminController.java

--- src/main/java/edu/arizona/csc536/book_lending_system/controller/AdminController.java
+++ src/main/java/edu/arizona/csc536/book_lending_system/controller/AdminController.java
@@ -9,8 +9,8 @@ import edu.arizona.csc536.book_lending_system.service.IBookCategoryService;
 import edu.arizona.csc536.book_lending_system.service.IUserService;
 import edu.arizona.csc536.book_lending_system.utils.page.Page;
 
-import jakarta.annotation.Resource;
-import jakarta.servlet.http.HttpServletRequest;
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
 
 import org.apache.ibatis.annotations.Param;
 import org.springframework.stereotype.Controller;
