4c8247bfc4b61bf793e32b3840827889f441040c
BourBon_TT <57243066+TT159@users.noreply.github.com>
2025-04-08 01:52:57 -0700

    Update BookController.java

--- src/main/java/edu/arizona/csc536/book_lending_system/controller/BookController.java
+++ src/main/java/edu/arizona/csc536/book_lending_system/controller/BookController.java
@@ -8,7 +8,11 @@ import edu.arizona.csc536.book_lending_system.service.IBookCategoryService;
 import edu.arizona.csc536.book_lending_system.service.IBookService;
 import edu.arizona.csc536.book_lending_system.utils.page.Page;
 
+//springboot 2
 import javax.annotation.Resource;
+
+// springboot 3:
+// import jakarta.annotation.Resource;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
