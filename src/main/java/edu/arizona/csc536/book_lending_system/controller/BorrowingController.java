6ce91b167c1be2bec69a7371d98764cdba3da849
BourBon_TT <57243066+TT159@users.noreply.github.com>
2025-04-07 18:37:51 -0700

    Update BorrowingController.java

--- src/main/java/edu/arizona/csc536/book_lending_system/controller/BorrowingController.java
+++ src/main/java/edu/arizona/csc536/book_lending_system/controller/BorrowingController.java
@@ -1,13 +1,29 @@
 package edu.arizona.csc536.book_lending_system.controller;
 
+import edu.arizona.csc536.book_lending_system.domain.Vo.BorrowingBooksVo;
 import edu.arizona.csc536.book_lending_system.service.IBorrowingBooksRecordService;
-import org.springframework.stereotype.Controller;
+import edu.arizona.csc536.book_lending_system.utils.page.Page;
 
-import javax.annotation.Resource;
+import jakarta.annotation.Resource;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class BorrowingController {
 
     @Resource
     private IBorrowingBooksRecordService borrowingBooksRecordService;
+
+    /**
+     * Show all users' borrowing records (admin view)
+     */
+    @RequestMapping("/allBorrowBooksRecordPage")
+    public String allBorrowingBooksRecordPage(Model model,
+            @RequestParam("pageNum") int pageNum) {
+        Page<BorrowingBooksVo> page = borrowingBooksRecordService.selectAllByPage(pageNum);
+        model.addAttribute("page", page);
+        return "admin/allBorrowingBooksRecord";
+    }
 }
