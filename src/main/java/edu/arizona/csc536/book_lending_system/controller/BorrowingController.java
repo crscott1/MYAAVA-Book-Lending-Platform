b75bc17ff41cbf0dfd1dcc05a02df72fb5a8ceea
BourBon_TT <57243066+TT159@users.noreply.github.com>
2025-04-08 01:53:34 -0700

    Update BorrowingController.java

--- src/main/java/edu/arizona/csc536/book_lending_system/controller/BorrowingController.java
+++ src/main/java/edu/arizona/csc536/book_lending_system/controller/BorrowingController.java
@@ -4,7 +4,11 @@ import edu.arizona.csc536.book_lending_system.domain.Vo.BorrowingBooksVo;
 import edu.arizona.csc536.book_lending_system.service.IBorrowingBooksRecordService;
 import edu.arizona.csc536.book_lending_system.utils.page.Page;
 
+//springboot 2
 import javax.annotation.Resource;
+
+//springboot 3
+// import jakarta.annotation.Resource;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
