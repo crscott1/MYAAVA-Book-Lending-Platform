f48e64cd12c128566d6b849bed1da3bad3dc0fab
xiyguo <303157088@qq.com>
2025-04-07 21:23:52 -0700

    update service/impl/UserServiceImpl and /service/impl/AdminServiceImpl

--- src/main/java/edu/arizona/csc536/book_lending_system/service/impl/AdminServiceImpl.java
+++ src/main/java/edu/arizona/csc536/book_lending_system/service/impl/AdminServiceImpl.java
@@ -14,31 +14,76 @@ import java.util.List;
 public class AdminServiceImpl implements IAdminService {
     @Override
     public boolean adminIsExist(String name) {
-        return false;
+        AdminExample adminExample = new AdminExample();
+        AdminExample.Criteria criteria = adminExample.createCriteria();
+        criteria.andAdminNameEqualTo(name);
+        List<Admin> admin = adminMapper.selectByExample(adminExample);
+        if (null == admin)
+            return false;
+        if (admin.size() < 1) {
+            return false;
+        }
+        return true;
     }
 
     @Override
     public Admin adminLogin(String name, String password) {
+
+        AdminExample adminExample = new AdminExample();
+        AdminExample.Criteria criteria = adminExample.createCriteria();
+        criteria.andAdminNameEqualTo(name);
+        List<Admin> admin = adminMapper.selectByExample(adminExample);
+
+        if (null == admin) {
+            return null;
+        }
+
+        for (Admin a : admin) {
+            if (a.getAdminPwd().equals(password)) {
+                return a;
+            }
+        }
         return null;
     }
 
     @Override
     public boolean addBook(Book book) {
+        int n = bookMapper.insert(book);
+        if (n > 0) {
+            return true;
+        }
         return false;
     }
 
     @Override
     public List<BookCategory> getBookCategories() {
-        return null;
+        BookCategoryExample bookCategoryExample = new BookCategoryExample();
+        return bookCategoryMapper.selectByExample(bookCategoryExample);
     }
 
     @Override
     public boolean addBookCategory(BookCategory bookCategory) {
+        int n = bookCategoryMapper.insert(bookCategory);
+        if (n > 0) {
+            return true;
+        }
         return false;
     }
 
     @Override
     public boolean updateAdmin(Admin admin, HttpServletRequest request) {
+        // get admin object from session
+        Admin sessionAdmin = (Admin) request.getSession().getAttribute("admin");
+        admin.setAdminId(sessionAdmin.getAdminId());
+        int n = adminMapper.updateByPrimaryKey(admin);
+
+        if (n > 0) {
+            //succeed to change，update session
+            Admin newAdmin = adminMapper.selectByPrimaryKey(admin.getAdminId());
+            request.getSession().setAttribute("admin", newAdmin);
+            return true;
+        }
+
         return false;
     }
 }
--- src/main/java/edu/arizona/csc536/book_lending_system/service/impl/UserServiceImpl.java
+++ src/main/java/edu/arizona/csc536/book_lending_system/service/impl/UserServiceImpl.java
@@ -11,51 +11,174 @@ import java.util.List;
 public class UserServiceImpl implements IUserService {
     @Override
     public List<User> findUserByUserName(String userName) {
-        return null;
+
+        UserExample userExample = new UserExample();
+        UserExample.Criteria criteria = userExample.createCriteria();
+
+        criteria.andUserNameEqualTo(userName);
+        List<User> users = userMapper.selectByExample(userExample);
+
+        return users;
     }
 
     @Override
     public User userLogin(String userName, String password) {
+
+        List<User> users = findUserByUserName(userName);
+
+        if (null == users) {
+            return null;
+        }
+
+        for (User user : users) {
+            if (user.getUserPwd().equals(password)) {
+                return user;
+            }
+        }
         return null;
     }
 
     @Override
     public boolean updateUser(User user, HttpServletRequest request) {
+        // get user obj from session
+        User sessionUser = (User) request.getSession().getAttribute("user");
+        user.setUserId(sessionUser.getUserId());
+
+        int n = userMapper.updateByPrimaryKey(user);
+
+        if (n > 0) {
+            //succeed to change，update session obj
+            User newUser = userMapper.selectByPrimaryKey(user.getUserId());
+            request.getSession().setAttribute("user", newUser);
+            return true;
+        }
+
         return false;
     }
 
+
     @Override
     public List<BorrowingBooksVo> findAllBorrowingBooks(HttpServletRequest request) {
-        return null;
+        User user = (User) request.getSession().getAttribute("user");
+
+        //setting query condition - userId
+        BorrowingBooksExample borrowingBooksExample = new BorrowingBooksExample();
+        BorrowingBooksExample.Criteria criteria = borrowingBooksExample.createCriteria();
+        criteria.andUserIdEqualTo(user.getUserId());
+        List<BorrowingBooks> borrowingBooksList = borrowingBooksMapper.selectByExample(borrowingBooksExample);
+        if (null == borrowingBooksList) {
+            return null;
+        }
+        // transform the Database object (Do) to View object (Vo)
+        List<BorrowingBooksVo> res = new LinkedList<BorrowingBooksVo>();
+        for (BorrowingBooks borrowingBooks : borrowingBooksList) {
+            Book book = bookMapper.selectByPrimaryKey(borrowingBooks.getBookId());
+            BorrowingBooksVo borrowingBooksVo = new BorrowingBooksVo();
+            borrowingBooksVo.setBook(book);
+
+            // transform date
+            Date date1 = borrowingBooks.getDate();
+            SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+            String dateOfBorrowing = sdf.format(date1);
+
+            // caculate the returning book date
+            Calendar calendar = Calendar.getInstance();
+            calendar.setTime(date1);
+            calendar.add(Calendar.MONTH, 2);//增加两个月
+            Date date2 = calendar.getTime();
+            String dateOfReturn = sdf.format(date2);
+
+            borrowingBooksVo.setDateOfBorrowing(dateOfBorrowing);
+            borrowingBooksVo.setDateOfReturn(dateOfReturn);
+            res.add(borrowingBooksVo);
+        }
+        return res;
     }
 
     @Override
     public boolean userReturnBook(int bookId, HttpServletRequest request) {
+        User user = (User) request.getSession().getAttribute("user");
+        BorrowingBooksExample borrowingBooksExample = new BorrowingBooksExample();
+        BorrowingBooksExample.Criteria criteria = borrowingBooksExample.createCriteria();
+        criteria.andUserIdEqualTo(user.getUserId());
+        criteria.andBookIdEqualTo(bookId);
+
+        // delete the records that have user_d==userId and book_id==bookId from database
+        int n = borrowingBooksMapper.deleteByExample(borrowingBooksExample);
+        if (n > 0) return true;
         return false;
     }
+
 
     @Override
     public boolean userBorrowingBook(int bookId, HttpServletRequest request) {
+        User user = (User) request.getSession().getAttribute("user");
+
+        // Check whether this book is accessible; check the status of this book in the borrowing book table
+        BorrowingBooksExample borrowingBooksExample = new BorrowingBooksExample();
+        BorrowingBooksExample.Criteria criteria = borrowingBooksExample.createCriteria();
+        criteria.andBookIdEqualTo(bookId);
+        List<BorrowingBooks> list = borrowingBooksMapper.selectByExample(borrowingBooksExample);
+
+        if (list.size() > 0) {
+            return false;
+        }
+
+        BorrowingBooks borrowingBooks = new BorrowingBooks();
+        borrowingBooks.setBookId(bookId);
+        borrowingBooks.setUserId(user.getUserId());
+        borrowingBooks.setDate(new Date());
+        int n = 0;
+
+
+        try {
+            // Insert a record of borrowing book (if fail return the failure of borrowing)
+            n = borrowingBooksMapper.insert(borrowingBooks);
+        } catch (Exception e) {
+            // e.printStackTrace();
+            return false;
+        }
+
+
+        if (n > 0) {
+            return true;
+        }
         return false;
     }
 
+
+
     @Override
     public User findUserById(int id) {
-        return null;
+        return userMapper.selectByPrimaryKey(id);
     }
 
     @Override
     public Page<User> findUserByPage(int pageNum) {
-        return null;
+        List<User> users = userMapper.selectByPageNum((pageNum - 1) * 10, 10);
+        Page<User> page = new Page<>();
+        page.setList(users);
+        page.setPageNum(pageNum);
+        page.setPageSize(10);
+
+        int userCount = userMapper.selectUserCount();
+        int pageCount = userCount / 10;
+        if (userCount % 10 != 0) {
+            pageCount++;
+        }
+        page.setPageCount(pageCount);
+        return page;
     }
+
 
     @Override
     public int insertUser(User user) {
-        return 0;
+        return userMapper.insert(user);
     }
+
 
     @Override
     public int deleteUserById(int userId) {
-        return 0;
+        return userMapper.deleteByPrimaryKey(userId);
     }
 }
