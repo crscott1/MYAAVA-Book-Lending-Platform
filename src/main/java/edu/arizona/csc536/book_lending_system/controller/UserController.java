b88898042edc42583575470cd5c1e5efcb383577
BourBon_TT <57243066+TT159@users.noreply.github.com>
2025-04-08 01:54:13 -0700

    Update UserController.java

--- src/main/java/edu/arizona/csc536/book_lending_system/controller/UserController.java
+++ src/main/java/edu/arizona/csc536/book_lending_system/controller/UserController.java
@@ -6,8 +6,13 @@ import edu.arizona.csc536.book_lending_system.service.IBookService;
 import edu.arizona.csc536.book_lending_system.service.IBorrowingBooksRecordService;
 import edu.arizona.csc536.book_lending_system.service.IUserService;
 
+// springboot 2
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
+
+// springboot 3
+// import jakarta.annotation.Resource;
+// import jakarta.servlet.http.HttpServletRequest;
 
 import org.apache.ibatis.annotations.Param;
 import org.springframework.stereotype.Controller;
