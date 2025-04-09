7a97641493ef47238f695978cfbcd5ad70c2efaa
BourBon_TT <57243066+TT159@users.noreply.github.com>
2025-04-08 20:50:13 -0700

    Update addBook.js

--- src/main/resources/static/scripts/admin/addBook.js
+++ src/main/resources/static/scripts/admin/addBook.js
@@ -7,8 +7,8 @@ layui.use(['element', 'form', 'layer'], function () {
 
         addBook();
 
-        // console.log(data.field); //当前容器的全部表单字段，名值对形式：{name: value};获取单个值data.field["title"]
-        return false; //阻止表单跳转。如果需要表单跳转，去掉这段即可。
+
+        return false; 
     });
 });
 
@@ -20,22 +20,20 @@ function addBook() {
         url: '/addBook',
         data: $('#addBookForm').serialize(),
         success: function (data) {
-            layer.msg("添加成功", {time: 1500, icon: 1});
+            layer.msg("Added successfully", {time: 1500, icon: 1});
 
             setTimeout(function () {
-                // 重新刷新页面
                 location.reload();
             }, 1500)
         },
         error: function (data) {
-            alert("添加失败");
+            alert("Add failed");
         }
     });
 };
 
 $(document).ready(function () {
 
-    //查找所有图书种类，并给select选择框赋值
     findAllBookCategory();
 
 });
@@ -49,7 +47,7 @@ function findAllBookCategory() {
         success: function (data) {
             console.log(data);
             $("select[name='bookCategory']").empty();
-            $("select[name='bookCategory']").append('<option value="">——请选择——</option>');
+            $("select[name='bookCategory']").append('<option value="">--- Please Select ---</option>');
             for (let i = 0; i < data.length; i++) {
                 let html = '<option value="' + data[i].categoryId + '">';
                 html += data[i].categoryName + '</option>';
