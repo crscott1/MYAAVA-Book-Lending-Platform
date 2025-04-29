da392b0e7da4e2cf032989072c73915d904f0403
han Han <11912937@mail.sustech.edu.cn>
2025-04-29 11:06:10 -0700

    add/delete category

--- src/main/resources/static/scripts/admin/addBookCategory.js
+++ src/main/resources/static/scripts/admin/addBookCategory.js
@@ -1,22 +1,48 @@
-layui.use(['form','element','layer'], function () {
+layui.use(['form', 'element', 'layer'], function () {
     let form = layui.form;
-    let element = layui.element();
+    let element = layui.element;
     let layer = layui.layer;
+    let $ = layui.jquery;
 
-    form.on('submit(btn_addBookCategory)', function (data) {
-        addBookCategory();
+    // 监听表单提交
+    form.on('submit(btn_addBookCategory)', function(data){
+        console.log("Click Add Category button，submit data：", data.field);
+        addBookCategory(data.field);
         return false;
     });
-});
 
-$(document).ready(function () {
+    // 添加分类的ajax
+    function addBookCategory(formData) {
+        $.ajax({
+            type: "POST",
+            url: "/addBookCategory",
+            data: formData,
+            dataType: "text", // 后端返回 "true" / "false"
+            success: function(response){
+                console.log("server return：", response);
+                if (response === "true") {
+                    layer.msg("Added successfully!", {icon: 1, time: 1500});
+                    setTimeout(function(){
+                        location.reload();
+                    }, 1500);
+                } else {
+                    layer.msg("Add failed!", {icon: 2, time: 1500});
+                }
+            },
+            error: function(xhr){
+                console.error("Add failed：", xhr.responseText);
+                layer.msg("Server error!", {icon: 2});
+            }
+        });
+    }
 
-    let lab1 = $("#lab1").html().trim();
-    let lab2 = $("#lab2").html().trim();
+    // 监听上一页、下一页
+    let lab1 = $("#lab1").html()?.trim();
+    let lab2 = $("#lab2").html()?.trim();
 
     $("#prePage").click(function () {
         if (lab1 == 1) {
-            layer.msg("This is the last page!", {icon: 7});
+            layer.msg("This is the first page!", {icon: 7});
             return false;
         }
         return true;
@@ -30,71 +56,45 @@ $(document).ready(function () {
         return true;
     });
 
-    $(".btn_deleteCategory").click(function () {
-
-
+    // 绑定删除按钮
+    $(document).on('click', '.btn_deleteCategory', function() {
         let that = $(this);
-        
+
         layer.confirm('Are you sure to delete?', {
             btn: ['Confirm', 'Cancel']
         }, function () {
             let bookCategoryId = that.val();
 
-            deleteBookCategoryById(bookCategoryId);
+            console.log("Ready to delete category,ID：", bookCategoryId);
 
-            that.parent().parent().remove();
-            layer.msg("Deleted successfully", {icon: 1, time: 1000});
-
-            setTimeout(function () {
-
-                parent.layer.closeAll();
-            }, 1000)
-
+            deleteBookCategoryById(bookCategoryId, that);
         });
-
     });
-});
 
-
-function addBookCategory() {
-    $.ajax({
-        async: false,
-        type: "post",
-        url: "/addBookCategory",
-        dataType: "json",
-        data: $("#addBookCategoryForm").serialize(),
-        success: function (data) {
-
-            if (data.toString() == "true") {
-                layer.msg("Added successfully!", {icon: 1, time: 1500});
-
-                setTimeout(function () {
-                    location.reload();
-                }, 1500);
-            } else {
-                layer.msg("Add failed!", {icon: 2, time: 1500});
+    // 删除分类的ajax
+    function deleteBookCategoryById(bookCategoryId, buttonElement) {
+        $.ajax({
+            type: "POST",
+            url: "/deleteCategory",
+            data: {bookCategoryId: bookCategoryId},
+            dataType: "text",
+            success: function (response) {
+                console.log("delete return:", response);
+                if (response === "true") {
+                    layer.msg("Deleted successfully!", {icon: 1, time: 1000});
+                    // 删除该行
+                    buttonElement.closest("tr").remove();
+                    setTimeout(function () {
+                        parent.layer.closeAll();
+                    }, 1000);
+                } else {
+                    layer.msg("Delete failed!", {icon: 2, time: 1000});
+                }
+            },
+            error: function (xhr) {
+                console.error("Delete failed!：", xhr.responseText);
+                layer.msg("Server error!", {icon: 2, time: 1000});
             }
-        },
-        error: function (data) {
-            layer.msg("Add failed!", {icon: 2, time: 1500});
-        }
-    });
-};
-
-
-function deleteBookCategoryById(bookCategoryId) {
-    $.ajax({
-        async: false,
-        type: "post",
-        url: "/deleteCategory",
-        dataType: "json",
-        data: {bookCategoryId: bookCategoryId},
-        success: function (data) {
-
-        },
-        error: function (data) {
-            alert(data.result);
-        }
-    });
-}
-
+        });
+    }
+});
